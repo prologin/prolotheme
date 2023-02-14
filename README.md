abf24e2b1ac8e79e318b6c44429c3e6a12a6f201
Dorian 'Renji' Péron <dorianperon.i@gmail.com>
2023-02-14 23:40:31 +0000

    codeblocks: refactor
    Squashed commit of the following:
    
    * refacto_code: Remove reference to deleted JS file
    
    * Apply Paul's suggestion in README
    
    * refacto_codeblocks: Add release note, Fix code quality
    
    * refacto_codeblocks: Update README, Formatting
    
    * refacto codeblocks: Remove code being duplicated due to templating,
    Move codeblocks' CSS to separate file.

--- README.md
+++ README.md
@@ -68,11 +68,11 @@ Pour l'instant, les langages suivants sont supportés:
 
 Dans le cas où en ajouter un nouveau serait intéressant, voici les étapes à suivre:
 
-1. Ajouter le langage dans LANGS dans `static/js/codemirror_global.js`.
+
+1. Ajouter dans `build.sh` la commande pour copier le fichier correspondant au language que vous voulez rajouter. 
+    La liste des languages est disponible ici : https://www-sop.inria.fr/teams/marelle/advanced-coq-16-17/jscoq/external/CodeMirror/mode/
 2. Ajouter le script du mode _CodeMirror_ correspondant dans `layouts/_default/single.html`
-3. Ajouter un fichier `layouts/_default/_markup/render-codeblock-code<langage>.html`. S'inspirer de ceux existant.
-4. Nommer le placeholder du code `code-<lang>-input-{{.Ordinal}}`. `{{.Ordinal}}` est une macro de Hugo qui est remplacée par son numéro de codeblock. C'est très important, car c'est ce qui permet au code de savoir le code de quel codeblock exécuter, et sur quel codeblock afficher la sortie.
-5. Dans la partie JS, récupérer les textAreas et output dont on a besoin, créer un objet CodeMirror (se référer au code existant), et ensuite enregistrer le block en suivant le nom `<lang>{{.Ordinal}}`.
-6. Dans `.../codemirror_global.js`, ajouter un commentaire `// === <lang> related ===` et créer la fonction `runItLang(content)`, où `content` est le deuxième argument passé à `addBlock()`.
-7. Toujours dans `.../codemirror_global.js`, ajouter un appel à la fonction `runItLang()` dans `runIt()`.
-8. Enfin, débugger pour fix les étapes que j'ai oubliées de citer.
+3. Ajouter un fichier `layouts/_default/_markup/render-codeblock-code<langage>.html`. S'inspirer de ceux existants.
+4. Dans `codeblocks.js`, ajouter un block `(function(){})()` et y définir les
+    fonctions `runCode<lang>()` et `initCode<lang>()`. Il faut également
+    appliquer `initCode<lang>()` à tous les blocks trouvés dans le document.
---
+++ static/js/codeblocks.js
@@ -0,0 +1,105 @@
+"use strict";
+
+function getCodeMirrorFromTxtArea(txtAreaNode, lang) {
+    return CodeMirror(
+        function (elt) {
+            txtAreaNode.parentNode.replaceChild(elt, txtAreaNode);
+        },
+        {
+            value: txtAreaNode.value,
+            mode: lang,
+            lineNumbers: true,
+            tabSize: 4,
+            theme: "monokai",
+        }
+    );
+
+}
+
+// Python code blocks
+(function () {
+
+    function runCodePython(inputCodeNode, outTxtNode, canvasNode) {
+        const code = inputCodeNode.getValue();
+
+        outTxtNode.innerHTML = "";
+        Sk.pre = "output";
+
+        Sk.configure({
+            // Output appends on the "codeOut" html element.
+            output: function (text) {
+                outTxtNode.innerHTML = outTxtNode.innerHTML + text;
+            },
+            read: builtinRead,
+        });
+
+        (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = canvasNode;
+        var myPromise = Sk.misceval.asyncToPromise(function () {
+            return Sk.importMainWithBody("<stdin>", false, code, true);
+        });
+        myPromise.then(
+            function (mod) {
+                console.log("success");
+            },
+            function (err) {
+                console.log("failed");
+                outTxtNode.innerText = err.toString();
+            }
+        );
+    }
+
+    function initCodePython(codeBlock) {
+        console.log("initCodePython");
+        const textArea = codeBlock.querySelector(".prolo-codeblock-input");
+        const outTxt = codeBlock.querySelector(".prolo-codepython-output");
+        const outCanvas = codeBlock.querySelector(".prolo-codepython-canvas");
+        const runBtn = codeBlock.querySelector(".prolo-codeblock-run-btn");
+
+        // Replace the text-area with a CodeMirror block,
+        // which includes line numbers and syntax highlighting.
+        const codeMirror = getCodeMirrorFromTxtArea(textArea, "python");
+
+        runBtn.addEventListener(
+            "click",
+            function() {runCodePython(codeMirror, outTxt, outCanvas);}
+        );
+
+    }
+
+    // Get all the Python code blocks and initialize them,
+    let pythonCodeBlocks = document.querySelectorAll(".prolo-codepython");
+    pythonCodeBlocks.forEach(initCodePython);
+})();
+
+// HTML code blocks
+(function (){
+    function runCodeHTML(inputCodeNode, outFrame) {
+        var code = inputCodeNode.getValue();
+        var out = outFrame.contentWindow.document;
+        out.open();
+        out.write("<!DOCTYPE html>"); // Added to shut up the "Quirks" warning.
+        out.write(code);
+        out.close();
+    }
+
+    function initCodeHTML(codeBlock) {
+        console.log("initCodeBlock");
+        const textArea = codeBlock.querySelector(".prolo-codeblock-input");
+        const outFrame = codeBlock.querySelector(".prolo-codeblock-result");
+        const runBtn = codeBlock.querySelector(".prolo-codeblock-run-btn");
+
+        // Replace the text-area with a CodeMirror block,
+        // which includes line numbers and syntax highlighting.
+        const codeMirror = getCodeMirrorFromTxtArea(textArea, "htmlmixed");
+
+        runBtn.addEventListener(
+            "click",
+            function() {runCodeHTML(codeMirror, outFrame);}
+        );
+
+    }
+
+    // Get all the HTML code blocks and initialize them,
+    let htmlCodeBlocks = document.querySelectorAll(".prolo-codehtml");
+    htmlCodeBlocks.forEach(initCodeHTML);
+})();
